d0c7421c7d03802c513a872cc5bb89599442ee77
Adriano Ferraz Lucati <ar_lucati@magazineluiza.intranet>
2022-09-07 10:44:53 -0300

    Criação do projeto DART

---
+++ crud_empresas/bin/crud_empresas.dart
@@ -0,0 +1,5 @@
+import 'package:crud_empresas/crud_empresas.dart' as crud_empresas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${crud_empresas.calculate()}!');
+}
---
+++ crud_empresas/test/crud_empresas_test.dart
@@ -0,0 +1,8 @@
+import 'package:crud_empresas/crud_empresas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
