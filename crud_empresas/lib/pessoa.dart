0275dc169947b640db486672dbcd967c41edf21b
Adriano Ferraz Lucati <adrianolucati@outlook.com>
2022-09-10 06:27:41 -0300

    métodos de CRUD da Abstract Class "pessoa"

--- crud_empresas/lib/pessoa.dart
+++ crud_empresas/lib/pessoa.dart
@@ -1,11 +1,78 @@
 abstract class Pessoa {
-  String id;
-  List String uuidEndereco;
-  List <String> uuidTelefone;
+  late String id;
+  List<String> uuidEndereco = [];
+  List<String> uuidTelefone = [];
 
-  Pessoa(
-    required this.id;
-    this.uuidEndereco;
-    this.uuidTelefone;
-  )
+  // Pessoa(
+  //   required this.id;
+  //   this.uuidEndereco;
+  //   this.uuidTelefone;
+  // )
+
+  void adicionarEndereco(String uuidEndereco) {
+    this.uuidEndereco.add(uuidEndereco);
+  }
+
+  void adicionarTelefone(String uuidTelefone) {
+    this.uuidTelefone.add(uuidTelefone);
+  }
+
+  void deletarTelefone(String uuidTelefone) {
+    final int qtde = this.uuidTelefone.length;
+
+    print('ID Telefone: $uuidTelefone');
+
+    this.uuidTelefone.remove(uuidTelefone);
+
+    // for (int i = 0; i < uuidTelefone.length; i++) {
+    //   if (this.uuidTelefone[i] == uuidTelefone) {
+    //     // remover telefone
+    //     return;
+    //   }
+    // }
+
+    qtde > uuidTelefone.length
+        ? print("Removido")
+        : print("UUID Não encontrado");
+  }
+
+  void deletarEndereco(String uuidEndereco) {
+    final int qtde = this.uuidEndereco.length;
+
+    print('ID Telefone: $uuidTelefone');
+
+    this.uuidEndereco.remove(uuidEndereco);
+
+    qtde > uuidTelefone.length
+        ? print("Removido")
+        : print("UUID Não encontrado");
+  }
+
+  void atualizarTelefone(String uuidTelefoneAntigo, String uuidTelefoneNovo) {
+    int index = -1;
+
+    index = uuidEndereco.indexOf(uuidTelefoneAntigo);
+
+    if (index >= 0) {
+      uuidEndereco[index] = uuidTelefoneNovo;
+      print(
+          "Endereço atualizado\nAntigo: $uuidTelefoneAntigo\nNovo: ${uuidEndereco[index]}");
+    } else {
+      print("ID do endereço antigo não foi localizado!");
+    }
+  }
+
+  void atualizarEndereco(String uuidEnderecoAntigo, String uuidEnderecoNovo) {
+    int index = -1;
+
+    index = uuidEndereco.indexOf(uuidEnderecoAntigo);
+
+    if (index >= 0) {
+      uuidEndereco[index] = uuidEnderecoNovo;
+      print(
+          "Endereço atualizado\nAntigo: $uuidEnderecoAntigo\nNovo: ${uuidEndereco[index]}");
+    } else {
+      print("ID do endereço antigo não foi localizado!");
+    }
+  }
 }
